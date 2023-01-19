34a837e87ddbffd235dfd599b7ffb8ccdb0b1809
naijel <naijelnaijelnm@gmail.com>
2023-01-19 21:53:47 +0530

    addeded

---
+++ todoproject/todoapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Task
+
+# Create your views here.
+
+def index(request):
+    task1 = Task.objects.all()
+    if request.method=='POST':
+        name=request.POST.get('task','')
+        priority=request.POST.get('priority','')
+        task=Task(name=name,priority=priority)
+        task.save()
+
+    return render(request,'home.html',{'task1':task1})
+
+
+#def details(request):
+
+   # return render(request,'detail.html',)
