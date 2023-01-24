7ce56952aa9d3507d1aff9477e4d57592f5e391d
njlnm <87595067+njlnm@users.noreply.github.com>
2023-01-24 20:22:32 +0530

    Add files via upload

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .import views
-app_name='todoapp'
+from . import views
+
+app_name = 'todoapp'
 urlpatterns = [
 
-    path('',views.index,name='home'),
-   # path('details',views.details,name='details')
-
+    path('', views.index, name='home'),
+    path('delete/<int:id>/', views.delete, name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('cvhome/', views.Tasklistview.as_view(), name='cvhome'),
+    path('cvdetail/<int:pk>/', views.TaskDetailView.as_view(), name='cvdetail'),
+    path('cvupdate/<int:pk>/', views.TaskUpdateView.as_view(), name='cvupdate'),
+    path('cvdelete/<int:pk>/', views.TaskDeleteView.as_view(), name='cvdelete'),
 
 ]
